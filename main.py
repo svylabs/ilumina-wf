02df7e9ff984cd12d29f95909dcea6ce7f61c9c7
svylabs <sg@svylabs.com>
2025-04-10 05:39:39 +0200

    Updating url

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ SECRET_PASSWORD = os.getenv("API_SECRET", "my_secure_password")
 PROJECT_ID = os.getenv("GCS_PROJECT_ID", "ilumina-451416")
 QUEUE_ID = os.getenv("TASK_QUEUE_ID", "analysis-tasks")
 LOCATION = os.getenv("TASK_LOCATION", "us-central1")
-TASK_HANDLER_URL = os.getenv("TASK_HANDLER_URL", "https://ilumina-451416.uc.r.appspot.com/api")
+TASK_HANDLER_URL = "https://ilumina-451416.uc.r.appspot.com/api"
 
 parent = tasks_client.queue_path(PROJECT_ID, LOCATION, QUEUE_ID)
 
