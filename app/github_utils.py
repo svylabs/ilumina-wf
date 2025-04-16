ca3dc136eb68430926c5e55f0ece341463684140
svylabs <sg@svylabs.com>
2025-04-16 22:23:58 +0200

    Using python request to check and create repository

--- app/github_utils.py
+++ app/github_utils.py
@@ -1,24 +1,23 @@
 import os
 import subprocess
+import requests
 
 def create_github_repo(token, username, repo_name):
     """Create a private GitHub repository."""
-    check_repo_command = (
-        f"curl -H \"Authorization: token {token}\" "
-        f"https://api.github.com/repos/{username}/{repo_name}"
-    )
-    repo_exists = os.system(check_repo_command)
-    output = subprocess.run(check_repo_command, shell=True, check=True, capture_output=True)
-    print(f"Repository exists: {repo_exists} {output.stdout.decode()}")
+    headers = {"Authorization": f"token {token}"}
+    check_repo_url = f"https://api.github.com/repos/{username}/{repo_name}"
 
-    if not repo_exists:
-        create_repo_command = (
-            f"curl -H \"Authorization: token {token}\" "
-            f"-d '{{\"name\": \"{repo_name}\", \"private\": true}}' "
-            f"https://api.github.com/user/repos"
-        )
-        print(f"Creating repository: {repo_name} {create_repo_command}")
-        os.system(create_repo_command)
+    response = requests.get(check_repo_url, headers=headers)
+    if response.status_code == 200:
+        print(f"Repository exists: {response.json()}")
+    else:
+        create_repo_url = "https://api.github.com/user/repos"
+        payload = {"name": repo_name, "private": True}
+        create_response = requests.post(create_repo_url, headers=headers, json=payload)
+        if create_response.status_code == 201:
+            print(f"Repository created: {create_response.json()}")
+        else:
+            print(f"Failed to create repository: {create_response.status_code}, {create_response.json()}")
 
 def set_github_repo_origin_and_push(repo_path, github_repo_url):
     """Set the origin of the repo and push to GitHub."""
