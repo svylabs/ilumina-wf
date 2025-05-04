4076f5f1c3a3b01ed8d160876e933443bf0f5c8b
svylabs <sg@svylabs.com>
2025-05-04 16:16:54 +0200

    Updating Dockerfile - remvoing nvm

---
+++ CONNECT.md
@@ -0,0 +1,6 @@
+gcloud app deploy
+gcloud app logs tail -s default
+gcloud app instances list
+gcloud app instances ssh aef-default-20250503t205803-abcd --service=default --version=20250503t205803
+sudo docker ps
+sudo docker exec -it <container_id> /bin/bash
--- scripts/compile_contracts.sh
+++ scripts/compile_contracts.sh
@@ -9,12 +9,18 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+echo "Compiling contracts in directory: $1"
+echo "Using Hardhat config file: $2"
+
 # Change to the specified directory
 cd "$1"
 
+rm -rf package-lock.json
+
 # Load NVM and switch to Node 20
 npm install --legacy-peer-deps
-npm install --save-dev @nomicfoundation/hardhat-ignition-ethers @nomicfoundation/hardhat-ignition @nomicfoundation/ignition-core @nomicfoundation/hardhat-verify @typechain/ethers-v6 ethers --legacy-peer-deps
+npm install --save-dev hardhat @nomicfoundation/hardhat-toolbox @nomicfoundation/hardhat-ignition-ethers @nomicfoundation/hardhat-ignition @nomicfoundation/ignition-core @nomicfoundation/hardhat-verify @typechain/ethers-v6 ethers --legacy-peer-deps
+#npm install -i --save-dev --force hardhat @nomicfoundation/hardhat-toolbox @nomicfoundation/hardhat-ignition-ethers @nomicfoundation/hardhat-ignition @nomicfoundation/ignition-core @nomicfoundation/hardhat-verify @typechain/ethers-v6 ethers
 
 # Run your Hardhat script (edit path as needed)
-npx hardhat compile --config "$2"
+./node_modules/.bin/hardhat compile --config "$2"
