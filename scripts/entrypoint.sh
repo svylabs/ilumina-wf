b428a0d252dffdbeae85ae5972c5aa2d004f71f2
svylabs <sg@svylabs.com>
2025-05-13 22:13:56 +0200

    Fixing script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if ["$MODE" == "server"]; then
+if [ "$MODE" == "server" ]; then
     echo "Starting server..."
-    source /app/.env
+    source .env
     source venv/bin/activate
     venv/bin/gunicorn -b 0.0.0.0:8080 main:app --timeout 900
-elif ["$MODE" == "runner"]; then
+elif [ "$MODE" == "runner" ]; then
     echo "Starting in background..."
-    source /app/.env
+    source .env
     source venv/bin/activate
     python3 simulation_runner_job.py
 else
