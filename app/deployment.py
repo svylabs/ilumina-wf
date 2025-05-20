b8d9058edce9b5b3b087df14a08534d1f85529aa
svylabs <sg@svylabs.com>
2025-05-20 17:36:43 +0200

    Adding a way to refine

--- app/deployment.py
+++ app/deployment.py
@@ -53,11 +53,22 @@ class DeploymentAnalyzer:
         with open(deployment_path, 'w') as f:
             json.dump(instructions.to_dict(), f, indent=2)
 
-    def analyze(self, user_prompt=None):
-        #deployable_contracts = self.identify_deployable_contracts()
-        project_summary = self.context.project_summary()
-        
-        prompt = f"""
+    def get_prompt_for_refinement(self, project_summary, existing_instructions, user_prompt=None):
+        return f"""
+        Here is the summary of the smart contract project:
+        {json.dumps(project_summary.to_dict())}  
+
+        -----
+
+        Here is the existing deployment instructions in json format:
+        {json.dumps(existing_instructions.to_dict())}
+
+        We need to refine the existing deployment instructions based on the user instructions below:
+        {user_prompt if user_prompt else "None"}
+        """
+    
+    def get_prompt_for_generating_deployment_instructions(self, project_summary, user_prompt=None):
+        return f"""
         Here is the summary of the smart contract project:
         {json.dumps(project_summary.to_dict())}  
 
@@ -71,11 +82,37 @@ class DeploymentAnalyzer:
         5: Repeat step 3-4 for each contract identified in step 1.
         6. Generate the deployment instructions.
 
-        Instructions from user:
+        Additional Instructions from user:
         {user_prompt if user_prompt else "None"}
         """
 
-        print(f"{user_prompt}")
+
+    def analyze(self, user_prompt=None):
+        #deployable_contracts = self.identify_deployable_contracts()
+        project_summary = self.context.project_summary()
+
+        self.context.deployment_instructions_path()
+        existing_instructions = None
+        refine = False
+        if os.path.exists(self.context.deployment_instructions_path()):
+            with open(self.context.deployment_instructions_path(), "r") as f:
+                content = json.loads(f.read())
+                existing_instructions = DeploymentInstruction.load(content)
+                refine = True
+
+        prompt = None
+        if refine:
+            prompt = self.get_prompt_for_refinement(
+                project_summary=project_summary,
+                existing_instructions=existing_instructions,
+                user_prompt=user_prompt)
+        else:
+            prompt = self.get_prompt_for_generating_deployment_instructions(
+                project_summary=project_summary,
+                user_prompt=user_prompt
+            )
+
+        #print(f"{prompt}")
 
         try:
             analyzer = ThreeStageAnalyzer(DeploymentInstruction)
